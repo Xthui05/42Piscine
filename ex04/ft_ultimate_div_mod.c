70f6f418771c10fcc063187914fca0e1aba2e0f5
Toh Xiao Hui <xiaohuitoh19@gmail.com>
2025-09-15 15:30:26 +0800

    first commit

---
+++ ex04/ft_ultimate_div_mod.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/14 21:36:41 by marvin            #+#    #+#             */
+/*   Updated: 2025/09/14 21:36:41 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void    ft_ptrnbr(int n)
+{
+    char    c;
+
+    if (n < 0)
+    {
+        write(1, "-", 1);
+        n = -n;
+    }
+
+    if(n >= 10)
+    {
+        ft_ptrnbr(n / 10);
+    }
+    c = (n % 10) +'0';
+    write(1, &c, 1);
+}
+
+void    ft_ultimate_div_mod(int *a, int *b)
+{
+    int div;
+    int mod;
+
+    div = (*a / *b);
+    mod = (*a % *b);
+
+    *a = div;
+    *b = mod;
+    write(1, "div: ", 5);
+    ft_ptrnbr(*a);
+    write(1, " | ", 3);
+    write(1, "mod: ", 5);
+    ft_ptrnbr(*b);
+}
+
+/*int main(void)
+{
+    int a;
+    int b;
+
+    a = 19;
+    b = 3;
+
+    ft_ultimate_div_mod(&a, &b);
+}*/
---
+++ ex06/ft_strlen.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/15 15:11:49 by marvin            #+#    #+#             */
+/*   Updated: 2025/09/15 15:11:49 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int ft_print_strlen(int n)
+{
+    char    c;
+
+    if (n >= 10)
+    {
+        ft_print_strlen(n / 10);
+    }
+    c = (n % 10) + '0';
+    write(1, &c, 1);
+}
+
+int ft_strlen(char *str)
+{
+    int i;
+    
+    i = 0;
+    while(str[i] != '\0')
+    {
+        i++;
+    }
+    return i;
+}
+
+void    ft_ptrstr_len(char *str)
+{
+    int length = ft_strlen(str);
+    ft_print_strlen(length);
+}
+
+/*int main(void)
+{
+    char    *s;
+    
+    s = "Hello 42KL!";
+    ft_strlen(s);
+    ft_ptrstr_len(s);
+    write(1, "\n", 1);
+    return(0);
+}*/
