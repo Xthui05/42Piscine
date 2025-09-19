0796a901f87de20c2c3aac3badb824c627dde8b0
Xthui05 <91778877+Xthui05@users.noreply.github.com>
2025-09-19 11:11:40 +0800

    Add files via upload

---
+++ C_04/ex00/ft_strlen.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: txiao-hu <txiao-hu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/18 14:57:09 by txiao-hu          #+#    #+#             */
+/*   Updated: 2025/09/18 14:57:53 by txiao-hu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+
+	if (argc != 2)
+		return (0);
+	i = ft_strlen(argv[1]);
+	printf("%i\n", i);
+}
