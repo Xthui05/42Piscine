7147140074487288fd39a1e8ecae60d0b1fc8077
Xthui05 <91778877+Xthui05@users.noreply.github.com>
2025-09-19 13:19:19 +0800

    Add files via upload
    Update revise files

--- C_04/ex02/ft_putnbr.c
+++ C_04/ex02/ft_putnbr.c
@@ -1,26 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+
+	+:+     */
+/*   By: txiao-hu <txiao-hu@student.42.fr>          +#+  +:+
+	+#+        */
+/*                                                +#+#+#+#+#+
+	+#+           */
+/*   Created: 2025/09/19 12:00:33 by txiao-hu          #+#    #+#             */
+/*   Updated: 2025/09/19 12:00:33 by txiao-hu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
+
 //#include <stdlib.h>
 
-void    ft_putnbr(int nb)
+void	ft_putnbr(int nb)
 {
-    char    c;
+	char c;
 
-    if (nb == -2147483648)
+	if (nb == -2147483648)
 	{
 		write(1, "-2147483648", 11);
 		return ;
 	}
-    if (nb < 0)
-    {
-        write(1, "-", 1);
-        nb = -nb;
-    }
-    if (nb >= 10)
-    {
-        ft_putnbr(nb / 10);
-    }
-    c = (nb % 10) + '0';
-    write(1, &c, 1);
+	if (nb < 0)
+	{
+		write(1, "-", 1);
+		nb = -nb;
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+	}
+	c = (nb % 10) + '0';
+	write(1, &c, 1);
 }
 
 // int main (int argc, char *argv[])
